ecc9b8407526c35948739bd82180588533bc721b
Richard Forrester <richard.forrester@gmail.com>
2023-05-19 12:13:42 +0800

    remove escape characters

--- system/uiText.ts
+++ system/uiText.ts
@@ -5,13 +5,13 @@ export const uiText = {
     when I began helping friends and family setup
     websites for their small businesses. From then
     on I was hooked.`,
-    aboutMeText2: `Currently I&apos;m working as a Full Stack Developer at`,
+    aboutMeText2: `${"Currently I'm working as a Full Stack Developer at"}`,
     aboutMeText3: `a small start up focused on developing innovative web3 applications`,
     currentCompanyTitle: "Omakasea",
-    currentTechStackText: "Here are a few technologies I&apos;ve been working with recently:",
+    currentTechStackText: `${"Here are a few technologies I've been working with recently:"}`,
     aboutMeTitle: "About Me",
     myNameIsText1: "Hi, my name is",
     myNameIsText2: "Richard Forrester.",
     checkoutMyResumeButtonText: "Check out my resume!",
-    whereIveWorkedText: "Where I&apos;ve Worked"
+    whereIveWorkedText: `${"Where I've Worked"}`,
 };
